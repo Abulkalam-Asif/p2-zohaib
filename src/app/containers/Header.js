a2b13ba663019bd972d0197d0a47d5eb1345aae7
Abulkalam-Asif <abulkalamasif1129@gmail.com>
2023-08-26 19:10:29 +0500

    Basic Setup

---
+++ src/app/containers/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <header className='max-w-8xl'>
+        header
+      </header>
+    </>
+  )
+}
+
+export default Header
