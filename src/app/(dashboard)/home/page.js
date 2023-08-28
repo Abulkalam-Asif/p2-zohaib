fc549f64933e390f7a6abc3c70363db5faa85a6d
Abulkalam-Asif <abulkalamasif1129@gmail.com>
2023-08-28 23:57:05 +0500

    Home page started

--- src/app/(dashboard)/home/page.js
+++ src/app/(dashboard)/home/page.js
@@ -1,9 +1,23 @@
+"use client"
+import HomeSearchBar from '@/app/components/HomeSearchBar';
+import MyBookmarks from '@/app/containers/MyBookmarks';
+import { useSidebarContext } from '@/app/contexts/SidebarContext';
 import React from 'react'
 
 const Page = () => {
+  const { isSidebarOut } = useSidebarContext();
+
   return (
     <>
-      <div>Home</div>
+      <div className={`min-h-screen bg-themeColor transition-all pt-20 pb-10 pr-8 ${isSidebarOut ? "pl-72 duration-100" : "pl-0 delay-200 duration-300"}`}>
+        <div className='max-w-4xl mx-auto space-y-12'>
+          <div className='space-y-8'>
+            <h1 className='text-4xl font-semibold text-center'>Good Morning, John</h1>
+            <HomeSearchBar />
+          </div>
+          <MyBookmarks />
+        </div>
+      </div>
     </>
   )
 }
